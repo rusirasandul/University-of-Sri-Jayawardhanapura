24017abba94bf54c613d0ad95c7d6196cff76b93
Rusira Sandul <139335602+rusirasandul@users.noreply.github.com>
2024-08-16 21:21:00 +0530

    Add files via upload

---
+++ Lab Sheet 1/Lab_Sheet1_Q5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+printf ("I am programmer !\n");
+printf("This is my first  C program.\n");
+printf("I'm excited to learn more!\n");
+printf("Programming is fun !\n");
+return 0;
+}
